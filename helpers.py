8094170da98947806cd495168b1e25e4f38ef2f5
Amir <amir.attoun@protonmail.ch>
2024-05-11 22:21:42 +0200

    Initital Commit.

---
+++ helpers.py
@@ -0,0 +1,87 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import csv
+from pathlib import Path
+from datetime import datetime
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+class Message(Exception):
+    def __init__(self, message: str):
+        self.message = message
+
+# Exception handler for CustomError
+@app.exception_handler(Message)
+def custom_error_handler(request: Request, exc: Message):
+    return templates.TemplateResponse("error.html", {
+        "request": request,  # Even though not used, it needs to be passed to TemplateResponse
+        "message": exc.message
+    }, status_code=400)
+
+def convert_date(timestamp):
+    # Convert the ISO 8601 format to dd-mm-yy
+    dt_object = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S")
+    return dt_object.strftime("%d-%m-%y")
+
+
+def normalize_data(data, mapping, standard_headers, config):
+    normalized_data = []
+    date_conversion_config = config.get('date_conversion')  # Get date conversion info from the config
+
+    for entry in data:
+        normalized_entry = {}
+        for source, standard in mapping.items():
+          #  print(f"Standard: {standard}")
+          #  print(f"Source: {source}")
+            #print(f"entry: {entry}")
+
+            if source in entry:
+                # Check if this field is a date needing conversion and if a conversion config exists
+                if source == "Date" and date_conversion_config:
+                    try:
+                        # Apply date conversion using the specified source and target formats
+                        dt_object = datetime.strptime(entry[source], date_conversion_config['source_format'])
+                        normalized_entry[standard] = dt_object.strftime(date_conversion_config['target_format'])
+                    except ValueError:
+                        # Handle cases where date conversion fails
+                        normalized_entry[standard] = None
+                else:
+                    # No conversion needed, transfer data as is
+                    normalized_entry[standard] = entry[source]
+                    # This never gets triggered?
+            else:
+                # Ensure field exists in normalized_entry, even if source is not in entry
+                normalized_entry[standard] = None
+
+
+        # Ensure all standard headers are present in the normalized_entry
+        for header in standard_headers:
+            if header not in normalized_entry:
+                normalized_entry[header] = None
+
+        normalized_data.append(normalized_entry)
+
+    return normalized_data
+
+
+def read_and_normalize_csv_data(file_path, bank, csv_configs, STANDARD_HEADERS):
+    config = csv_configs.get(bank)
+    if not config:
+        raise Message(f"You have not selected a valid bank!")
+
+    with open(file_path, newline='', encoding='iso-8859-1') as csvfile:
+        if config['headers']:
+            reader = csv.DictReader(csvfile, fieldnames=config['headers'], delimiter=config['delimiter'])
+            if config['skip_first_row']:
+                next(reader)  # Skip if it's just data
+        else:
+            reader = csv.DictReader(csvfile, delimiter=config['delimiter'])  # Headers in file
+
+        data = [row for row in reader]
+
+    normalized_data = normalize_data(data, config['mapping'], STANDARD_HEADERS, config)
+    return normalized_data
---
+++ main.py
@@ -0,0 +1,114 @@
+from fastapi import FastAPI, Request, UploadFile, Form
+from pathlib import Path
+from fastapi.responses import HTMLResponse, RedirectResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import csv, io, os
+
+from helpers import app, templates, Message, read_and_normalize_csv_data
+
+# Constants
+# Path() --> Root Directory
+UPLOAD_DIR = Path() / "uploads"
+BANKS = ["Migrosbank", "Viseca (One)"]
+FILETYPES = [".CSV", ".csv"]
+
+
+STANDARD_HEADERS = ["Datum", "Buchungstext", "Betrag", "Valuta"]
+
+CSV_CONFIGS = {
+    'Migrosbank': {
+        'headers': ["Datum", "Buchungstext", "Betrag", "Valuta"],
+        'delimiter': ';',
+        'skip_first_row': True,
+        'mapping': {"Datum": "Datum", "Buchungstext": "Buchungstext", "Betrag": "Betrag", "Valuta": "Valuta"},
+        'date_conversion': None,
+        'type': "Debit"
+    },
+    'Viseca (One)': {
+        'headers': ["TransactionID", "Date", "Merchant", "Amount", "PFMCategoryID", "PFMCategoryName"],
+        'delimiter': ',',
+        'skip_first_row': True,
+        'mapping': {"Date": "Valuta", "Merchant": "Payee", "Amount": "Betrag"},
+        'date_conversion': {"source_format": "%Y-%m-%dT%H:%M:%S", "target_format": "%d.%m.%y"},
+        'type': "Credit"
+    }
+    # Other configurations as needed
+}
+
+
+# Root Explorer, pass BANKS for valid banks
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"banks": BANKS}
+    )
+
+# Upload CSV, validate for valid input (not empty and must be a csv)
+@app.post("/uploadfile")
+async def create_upload_file(file_upload: UploadFile, request: Request):
+    # Get the selected bank
+    form_data = await request.form()
+    bank = form_data.get("banks")
+
+    data = await file_upload.read()
+    # Check if a file was provided
+    if not data:
+         raise Message("You did not provide any file!")
+    elif not file_upload.filename.lower().endswith(tuple(FILETYPES)):
+         raise Message("You did not provide a .csv file!")
+    else:
+
+        # TODO CHECK IF VALID CSV
+
+        save_to = UPLOAD_DIR / file_upload.filename
+        with open(save_to, 'wb') as f:
+                f.write(data)
+
+        # Open the file and pass it to verify
+
+        #Read the csv
+        filedata = read_and_normalize_csv_data(UPLOAD_DIR / file_upload.filename, bank, CSV_CONFIGS, STANDARD_HEADERS)
+
+        config = CSV_CONFIGS.get(bank)
+        account_type = config.get('type')
+
+        return templates.TemplateResponse(
+        request=request, name="verify.html", context={"filedata": filedata, "account_type": account_type}
+        )
+
+@app.post("/download-file")
+async def download_file(request: Request):
+    form_data = await request.form()
+    filedata = []
+
+    # Extract the data from the form
+    i = 1
+    while f"buchungstext_{i}" in form_data:
+        outflow = form_data.get(f"outflow_{i}")
+        if outflow and outflow.startswith('-'):
+            outflow = outflow.lstrip('-')
+        entry = {
+            "Date": form_data.get(f"valuta_{i}"),
+            "Payee": form_data.get(f"payee_{i}"),
+            "Memo": form_data.get(f"buchungstext_{i}"),
+            "Outflow": outflow,
+            "Inflow": form_data.get(f"inflow_{i}")
+        }
+        filedata.append(entry)
+        i += 1
+
+    # Write the data to a new CSV file
+    filename = "updated_data.csv"
+    with open(filename, "w", newline="", encoding="iso-8859-1") as f:
+        if filedata:
+            writer = csv.DictWriter(f, fieldnames=filedata[0].keys())
+            writer.writeheader()
+            writer.writerows(filedata)
+        else:
+            raise Message("No data to write to CSV file")
+
+    # TODO Delete file on server
+
+    # Return a FileResponse to start the download
+    return FileResponse(filename, media_type="text/csv", filename=os.path.basename(filename))
