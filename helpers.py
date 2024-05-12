c51d6946608963d5978e5e118454eb4562b2e55e
AmirAttoun <amir.attoun@protonmail.ch>
2024-05-12 10:41:10 +0200

    * HEADER CUTOFF CONFIG * BACKGROUND CLEANUP TASK

--- helpers.py
+++ helpers.py
@@ -1,29 +1,36 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-import csv
-from pathlib import Path
+import csv, os
 from datetime import datetime
 
-#----- TODO Summary -------
-# Adjust Migrosbank header cut off
+# ----- TODO Summary -------
+# [X] Adjust Migrosbank header cut off
+# Make sure the csv is validates for header length
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 class Message(Exception):
     def __init__(self, message: str):
         self.message = message
 
+
 # Exception handler for CustomError
 @app.exception_handler(Message)
 def custom_error_handler(request: Request, exc: Message):
-    return templates.TemplateResponse("error.html", {
-        "request": request,  # Even though not used, it needs to be passed to TemplateResponse
-        "message": exc.message
-    }, status_code=400)
+    return templates.TemplateResponse(
+        "error.html",
+        {
+            "request": request,  # Even though not used, it needs to be passed to TemplateResponse
+            "message": exc.message,
+        },
+        status_code=400,
+    )
+
 
 def convert_date(timestamp):
     # Convert the ISO 8601 format to dd-mm-yy
@@ -31,9 +38,16 @@ def convert_date(timestamp):
     return dt_object.strftime("%d-%m-%y")
 
 
+def delete_file(name: str):
+    """Background task function for deleting a file."""
+    os.remove(name)
+
+
 def normalize_data(data, mapping, standard_headers, config):
     normalized_data = []
-    date_conversion_config = config.get('date_conversion')  # Get date conversion info from the config
+    date_conversion_config = config.get(
+        "date_conversion"
+    )  # Get date conversion info from the config
 
     for entry in data:
         normalized_entry = {}
@@ -43,8 +57,12 @@ def normalize_data(data, mapping, standard_headers, config):
                 if source == "Date" and date_conversion_config:
                     try:
                         # Apply date conversion using the specified source and target formats
-                        dt_object = datetime.strptime(entry[source], date_conversion_config['source_format'])
-                        normalized_entry[standard] = dt_object.strftime(date_conversion_config['target_format'])
+                        dt_object = datetime.strptime(
+                            entry[source], date_conversion_config["source_format"]
+                        )
+                        normalized_entry[standard] = dt_object.strftime(
+                            date_conversion_config["target_format"]
+                        )
                     except ValueError:
                         # Handle cases where date conversion fails
                         normalized_entry[standard] = None
@@ -55,7 +73,6 @@ def normalize_data(data, mapping, standard_headers, config):
             else:
                 # Ensure field exists in normalized_entry, even if source is not in entry
                 normalized_entry[standard] = None
-
 
         # Ensure all standard headers are present in the normalized_entry, otherwise set to empty string
         for header in standard_headers:
@@ -74,15 +91,25 @@ def read_and_normalize_csv_data(file_path, bank, csv_configs, STANDARD_HEADERS):
 
     # TODO if Migrosbank cut off a certain amount of lines
 
-    with open(file_path, newline='', encoding='iso-8859-1') as csvfile:
-        if config['headers']:
-            reader = csv.DictReader(csvfile, fieldnames=config['headers'], delimiter=config['delimiter'])
-            if config['skip_first_row']:
+    with open(file_path, newline="", encoding="iso-8859-1") as csvfile:
+        if config["headers"]:
+            reader = csv.DictReader(
+                csvfile, fieldnames=config["headers"], delimiter=config["delimiter"]
+            )
+            # First, we skip the amount of rows defined in config['header_cutoff']
+            # TODO Validate that there is a valid CSV (length of header)
+            for i in range(config["header_cutoff"]):
+                next(reader)
+
+            # Here we are skipping the first rows if it's a header
+            if config["skip_first_row"]:
                 next(reader)  # Skip if it's just data
         else:
-            reader = csv.DictReader(csvfile, delimiter=config['delimiter'])  # Headers in file
+            reader = csv.DictReader(
+                csvfile, delimiter=config["delimiter"]
+            )  # Headers in file
 
         data = [row for row in reader]
 
-    normalized_data = normalize_data(data, config['mapping'], STANDARD_HEADERS, config)
+    normalized_data = normalize_data(data, config["mapping"], STANDARD_HEADERS, config)
     return normalized_data
--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from fastapi import FastAPI, Request, UploadFile, Form
+from fastapi import Request, UploadFile, BackgroundTasks
 from pathlib import Path
-from fastapi.responses import HTMLResponse, RedirectResponse, FileResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import csv, io, os
+from fastapi.responses import HTMLResponse, FileResponse
+import csv, os
 
-from helpers import app, templates, Message, read_and_normalize_csv_data
+from helpers import app, templates, Message, read_and_normalize_csv_data, delete_file
 
-# TODO Summary
-# TODO Validate if valid CSV (Migrosbank or Viseca)
-# TODO Delete File on server
+# TODO Validate if valid CSV (Migrosbank or Viseca) []
+# TODO Delete File on server [X]
+# Use background task to delete file
+# Verify Usage of BackgroundTasks
 
 
 # Constants
 # Path() --> Root Directory
 UPLOAD_DIR = Path() / "uploads"
+CONVERT_DIR = Path() / "converted"
 BANKS = ["Migrosbank", "Viseca (One)"]
 FILETYPES = [".CSV", ".csv"]
 
@@ -22,21 +22,43 @@ FILETYPES = [".CSV", ".csv"]
 STANDARD_HEADERS = ["Datum", "Buchungstext", "Betrag", "Valuta"]
 
 CSV_CONFIGS = {
-    'Migrosbank': {
-        'headers': ["Datum", "Buchungstext", "Betrag", "Valuta"],
-        'delimiter': ';',
-        'skip_first_row': True,
-        'mapping': {"Datum": "Datum", "Buchungstext": "Buchungstext", "Betrag": "Betrag", "Valuta": "Valuta"},
-        'date_conversion': None,
-        'type': "Debit"
+    "Migrosbank": {
+        "headers": ["Datum", "Buchungstext", "Betrag", "Valuta"],
+        "delimiter": ";",
+        "skip_first_row": True,
+        "mapping": {
+            "Datum": "Datum",
+            "Buchungstext": "Buchungstext",
+            "Betrag": "Betrag",
+            "Valuta": "Valuta",
+        },
+        "date_conversion": None,
+        "type": "Debit",
+        "header_cutoff": 12,
     },
-    'Viseca (One)': {
-        'headers': ["TransactionID", "Date", "Merchant", "Amount", "PFMCategoryID", "PFMCategoryName"],
-        'delimiter': ',',
-        'skip_first_row': True,
-        'mapping': {"Date": "Valuta", "Merchant": "Payee", "Amount": "Betrag"},
-        'date_conversion': {"source_format": "%Y-%m-%dT%H:%M:%S", "target_format": "%d.%m.%y"},
-        'type': "Credit"
+    "Viseca (One)": {
+        "headers": [
+            "TransactionID",
+            "Date",
+            "Merchant",
+            "Amount",
+            "PFMCategoryID",
+            "PFMCategoryName",
+        ],
+        "delimiter": ",",
+        "skip_first_row": True,
+        "mapping": {
+            "Date": "Valuta",
+            "Merchant": "Payee",
+            "Amount": "Betrag",
+            "PFMCategoryName": "Buchungstext",
+        },
+        "date_conversion": {
+            "source_format": "%Y-%m-%dT%H:%M:%S",
+            "target_format": "%d.%m.%y",
+        },
+        "type": "Credit",
+        "header_cutoff": 0,
     }
     # Other configurations as needed
 }
@@ -49,6 +71,7 @@ async def index(request: Request):
         request=request, name="index.html", context={"banks": BANKS}
     )
 
+
 # Upload CSV, validate for valid input (not empty and must be a csv)
 @app.post("/uploadfile")
 async def create_upload_file(file_upload: UploadFile, request: Request):
@@ -59,31 +82,35 @@ async def create_upload_file(file_upload: UploadFile, request: Request):
     data = await file_upload.read()
     # Check if a file was provided
     if not data:
-         raise Message("You did not provide any file!")
+        raise Message("You did not provide any file!")
     elif not file_upload.filename.lower().endswith(tuple(FILETYPES)):
-         raise Message("You did not provide a .csv file!")
+        raise Message("You did not provide a .csv file!")
     else:
-
         # TODO CHECK IF VALID CSV
 
         save_to = UPLOAD_DIR / file_upload.filename
-        with open(save_to, 'wb') as f:
-                f.write(data)
+        with open(save_to, "wb") as f:
+            f.write(data)
 
         # Open the file and pass it to verify
 
-        #Read the csv
-        filedata = read_and_normalize_csv_data(UPLOAD_DIR / file_upload.filename, bank, CSV_CONFIGS, STANDARD_HEADERS)
-
-        config = CSV_CONFIGS.get(bank)
-        account_type = config.get('type')
-
-        return templates.TemplateResponse(
-        request=request, name="verify.html", context={"filedata": filedata, "account_type": account_type}
+        # Read the csv
+        filedata = read_and_normalize_csv_data(
+            UPLOAD_DIR / file_upload.filename, bank, CSV_CONFIGS, STANDARD_HEADERS
         )
 
+        config = CSV_CONFIGS.get(bank)
+        account_type = config.get("type")
+
+        return templates.TemplateResponse(
+            request=request,
+            name="verify.html",
+            context={"filedata": filedata, "account_type": account_type},
+        )
+
+
 @app.post("/download-file")
-async def download_file(request: Request):
+async def download_file(request: Request, background_tasks: BackgroundTasks):
     form_data = await request.form()
     filedata = []
 
@@ -91,20 +118,21 @@ async def download_file(request: Request):
     i = 1
     while f"buchungstext_{i}" in form_data:
         outflow = form_data.get(f"outflow_{i}")
-        if outflow and outflow.startswith('-'):
-            outflow = outflow.lstrip('-')
+        if outflow and outflow.startswith("-"):
+            outflow = outflow.lstrip("-")
         entry = {
             "Date": form_data.get(f"valuta_{i}"),
             "Payee": form_data.get(f"payee_{i}"),
             "Memo": form_data.get(f"buchungstext_{i}"),
             "Outflow": outflow,
-            "Inflow": form_data.get(f"inflow_{i}")
+            "Inflow": form_data.get(f"inflow_{i}"),
         }
         filedata.append(entry)
         i += 1
 
     # Write the data to a new CSV file
-    filename = "updated_data.csv"
+    filename = CONVERT_DIR / "updated_data.csv"
+
     with open(filename, "w", newline="", encoding="iso-8859-1") as f:
         if filedata:
             writer = csv.DictWriter(f, fieldnames=filedata[0].keys())
@@ -113,7 +141,13 @@ async def download_file(request: Request):
         else:
             raise Message("No data to write to CSV file")
 
-    # TODO Delete file on server
-
+    # When FileResponse is returned, it will trigger a download, and the file will be deleted afterwards
     # Return a FileResponse to start the download
-    return FileResponse(filename, media_type="text/csv", filename=os.path.basename(filename))
+    response = FileResponse(
+        filename, media_type="text/csv", filename=os.path.basename(filename)
+    )
+
+    # Delete file on server, using background task
+    background_tasks.add_task(delete_file, filename)
+
+    return response
